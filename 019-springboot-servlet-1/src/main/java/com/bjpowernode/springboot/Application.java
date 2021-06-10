0e68dddec832b1484a29c0c72feeb9e72b159662
godliang <2426534657@qq.com>
2021-06-10 14:16:30 +0800

    springboot框架下使用Servlet 注解方式 --> @WebServlet,@ServletComponentSan

---
+++ 019-springboot-servlet-1/src/main/java/com/bjpowernode/springboot/Application.java
@@ -0,0 +1,17 @@
+package com.bjpowernode.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication //开启spring配置
+
+//扫描@WebServlet
+@ServletComponentScan(basePackages = "com.bjpowernode.springboot.servlet")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
